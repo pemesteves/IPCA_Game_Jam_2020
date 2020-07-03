69856e82d9cbf89a25b05a04d00bd5791562f406
Pedro Esteves <up201705160@fe.up.pt>
2020-07-03 22:00:34 +0100

    Added player and camera movement script

--- IPCA_Game_Jam/Assets/Scripts/Player.cs
+++ IPCA_Game_Jam/Assets/Scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator playerAnimator;
+    private bool isWalking;
+    private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isWalking = false;
+        playerAnimator = gameObject.GetComponent<Animator>();
+        speed = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+        {
+            if (!isWalking)
+            {
+                isWalking = true;
+                playerAnimator.SetBool("isWalking", isWalking);
+            }
+
+            float z = Input.GetAxis("Vertical")*speed;
+            float x = Input.GetAxis("Horizontal")*speed;
+            Walk(x, z);
+        }
+        else
+        {
+            if (isWalking)
+            {
+                isWalking = false;
+                playerAnimator.SetBool("isWalking", isWalking);
+            }
+        }
+
+    }
+
+    private void Walk(float x, float z)
+    {
+        gameObject.transform.Translate(new Vector3(x, 0, z));
     }
 }

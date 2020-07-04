0cca3648ff55ce006e050555026315dcc4cdebd3
Pedro Esteves <up201705160@fe.up.pt>
2020-07-04 01:29:58 +0100

    Added bullet

---
+++ IPCA_Game_Jam/Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(-transform.right*10);
+
+        //If a bullet doesn't hit anything in 2 seconds, it'll be destroyed
+        Invoke("Destroy", 2); 
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy();
+    }
+}

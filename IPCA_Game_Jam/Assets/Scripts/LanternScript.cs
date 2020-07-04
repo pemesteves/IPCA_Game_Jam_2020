57f1057cf9a422441e0fcf276f7b389ba0c82225
Marcia Isabel Reis Teixeira <up201706065@fe.up.pt>
2020-07-04 18:05:59 +0100

    Added lantern fade out script

---
+++ IPCA_Game_Jam/Assets/Scripts/LanternScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternScript : MonoBehaviour
+{
+
+    public Light lightToFade;
+    public float fadeTime = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fadeOut(lightToFade, fadeTime));   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator fadeOut(Light lightToFade, float duration)
+    {
+        float minLuminosity = 0;
+        float maxLuminosity = 2.3f; 
+
+        float counter = 0f;
+
+        float currentIntensity = lightToFade.intensity;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+
+            lightToFade.intensity = Mathf.Lerp(maxLuminosity, minLuminosity, counter / duration);
+
+            yield return null;
+        }
+    }
+
+
+}

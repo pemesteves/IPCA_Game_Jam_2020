9bee025d886d55c55ba396ac9f782144fcd99594
Pedro Esteves <up201705160@fe.up.pt>
2020-07-04 21:02:33 +0100

    Added Game Over Screen

--- IPCA_Game_Jam/Assets/Scripts/GameController.cs
+++ IPCA_Game_Jam/Assets/Scripts/GameController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject gameOverScreen;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,27 @@ public class GameController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GameOver()
+    {
+        GameObject gameObject = GameObject.FindGameObjectWithTag("Player");
+        //Disable Player Script
+        gameObject.GetComponent<Player>().enabled = false;
+
+        //Disable Camera Script
+        Camera.main.GetComponent<CameraMovement>().enabled = false;
+
+        gameOverScreen.SetActive(true);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }

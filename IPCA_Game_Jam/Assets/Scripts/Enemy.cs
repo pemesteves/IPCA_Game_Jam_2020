89ce38fc2ebc22c92c00ada5c4416fdac5878ff0
Henrique Santos <up201706898@fe.up.pt>
2020-07-04 18:23:28 +0100

    Added enemy animations

--- IPCA_Game_Jam/Assets/Scripts/Enemy.cs
+++ IPCA_Game_Jam/Assets/Scripts/Enemy.cs
@@ -8,11 +8,13 @@ public class Enemy : MonoBehaviour
     public float lookRadius = 10f;
     public Transform target;
     NavMeshAgent agent;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = transform.GetChild(0).gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,13 +23,21 @@ public class Enemy : MonoBehaviour
         float distance = Vector3.Distance(target.position, transform.position);
 
         if(distance <= lookRadius) {
+            agent.isStopped = false;
             agent.SetDestination(target.position);
+            animator.SetBool("isAgro", true);
 
             if(distance <= agent.stoppingDistance) {
+                animator.SetBool("isAttacking", true);
                 Vector3 direction = (target.position - transform.position).normalized;
                 Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+                transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * 5);
+            } else {
+                animator.SetBool("isAttacking", false);
             }
+        } else {
+            animator.SetBool("isAgro", false);
+            agent.isStopped = true;
         }
     }
 

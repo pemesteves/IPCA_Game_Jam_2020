c9e60d267916a92b42333127f1651afc4234d805
Pedro Esteves <up201705160@fe.up.pt>
2020-07-05 02:37:02 +0100

    Added menu scene

---
+++ IPCA_Game_Jam/Assets/Scripts/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject instructions;
+    public GameObject credits;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Instructions()
+    {
+        menu.SetActive(false);
+        instructions.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void Credits()
+    {
+        menu.SetActive(false);
+        instructions.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void Menu()
+    {
+        menu.SetActive(true);
+        instructions.SetActive(false);
+        credits.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}

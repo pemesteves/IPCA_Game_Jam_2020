b1b8246d352184eaceeada87cf6fd0f6283f587e
Pedro Esteves <up201705160@fe.up.pt>
2020-07-05 13:08:10 +0100

    Added player sounds

--- IPCA_Game_Jam/Assets/Scripts/Player.cs
+++ IPCA_Game_Jam/Assets/Scripts/Player.cs
@@ -16,6 +16,9 @@ public class Player : MonoBehaviour
 
     private float speed;
 
+    /* SOUNDS */
+    public AudioSource walkingSound;
+
     /* SHOOTING */
     private bool canShoot;
     private GameObject gun;
@@ -55,6 +58,7 @@ public class Player : MonoBehaviour
         {
             if (!isWalking)
             {
+                walkingSound.Play();
                 isWalking = true;
                 playerAnimator.SetBool("isWalking", isWalking);
             }
@@ -67,6 +71,7 @@ public class Player : MonoBehaviour
         {
             if (isWalking)
             {
+                walkingSound.Stop();
                 isWalking = false;
                 playerAnimator.SetBool("isWalking", isWalking);
             }
